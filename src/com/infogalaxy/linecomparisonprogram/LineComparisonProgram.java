a4e0e6b4c048d9ee951ff7b5ad6defee9246f7b0
sejaltamboli <sejaltamboli0602@gmail.com>
2022-03-10 21:19:23 -0800

    ADDED: Line Comparison Computation Program Java Code

---
+++ src/com/infogalaxy/linecomparisonprogram/LineComparisonProgram.java
@@ -0,0 +1,8 @@
+package com.infogalaxy.linecomparisonprogram;
+
+public class LineComparisonProgram {
+    public static void main(String[]args){
+        //Welcome message for User
+        System.out.println("Welcome to Line Comparison Computation Program Developed By Sejal Tamboli.");
+    }
+}
